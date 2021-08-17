3618eec51657c94974abc60b7f5dc3a461aa7345
Rishi Ghan <rishi.ghan@gmail.com>
2021-08-17 09:51:31 -0700

    v1.0.3

--- index.d.ts
+++ index.d.ts
@@ -69,3 +69,41 @@ export interface ISharpResizedImageStats {
   premultiplied: boolean;
   size: number;
 }
+
+
+// AirDCPP search API types
+
+export interface SearchQuery {
+  pattern: string;
+  file_type: string | null;
+  min_size: number | null;
+  max_size: number | null;
+  extensions: string[] | null;
+  excluded: string[] | null;
+}
+
+export interface SearchInstance {
+  id: number;
+  owner: string;
+  expires_in: number;
+  current_search_id: number;
+  queue_time: number;
+  queued_count: number;
+  result_count: number;
+  searches_sent_ago: number;
+  query?: SearchQuery;
+}
+
+export interface SearchResponse {
+  queue_time: number;
+  search_id: number;
+  queued_count: number;
+}
+
+export const enum PriorityEnum {
+  LOWEST = 2,
+  LOW = 3,
+  NORMAL = 4,
+  HIGH = 5,
+  HIGHEST = 6,
+}
